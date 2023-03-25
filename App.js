cb1baaec4d39de2d36247686ba55a80c0b830859
Andrés Rengifo <andresm.rengifo@gmail.com>
2023-03-25 09:25:01 -0500

    .

--- App.js
+++ App.js
@@ -327,7 +327,7 @@ export default function App() {
   const styles = StyleSheet.create({
     container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#252525',
     alignItems: 'center',
     justifyContent: 'start  ',
     gap: 10 
